b006e874a0c766eaa1ba179f8da069d6a7ec8135
alekseyaq <kuznetz.vrn@gmail.com>
2017-05-28 11:14:24 -0400

    temp commit

--- solutions/a_temple_of_snakes.py
+++ solutions/a_temple_of_snakes.py
@@ -2,75 +2,59 @@ import math
 from itertools import chain
 
 
-def higher_than_ideal(length, line, ideal_ground):
-    return all(block >= ideal_block for block, ideal_block in zip(line, ideal_ground))
+def create_ideal_ground(length):
+    middle = math.ceil(length / 2)
+    return list(chain(range(1, middle + 1), range(middle - 1, 0, -1)))
 
-def ideal_ground_length(length):
-    if length % 2:
-        return length - 1
+
+def higher_than_ideal(line, ideal_ground):
+    return all(block >= ideal_block
+               for block, ideal_block in zip(line, ideal_ground))
+
+
+def create_list_of_lines(line, new_length):
+    new_list_of_lines = []
+    length = len(line)
+    length_difference = length - new_length
+    for i in range(length_difference):
+        new_list_of_lines.append(line[i:-length_difference+i])
+    new_list_of_lines.append(line[-new_length:])
+    return new_list_of_lines
+
+
+def create_params(length, line):
+    if length % 2 == 0:
+        new_length = length - 1
     else:
-        return length - 2
+        new_length = length - 2
+    list_of_lines = create_list_of_lines(line, new_length)
+    return new_length, list_of_lines
+
 
 def number_of_operations(length, line):
-    middle = math.ceil(length / 2)
-    ideal_ground = list(chain(range(1, middle + 1), range(middle - 1, 0, -1)))
+    ideal_ground = create_ideal_ground(length)
     line = list(map(int, line.split(' ')))
-    if length % 2:
-        if higher_than_ideal(length, line, ideal_ground):
-            return sum(line) - sum(ideal_ground)
-        else:
-            length -= 2
-            middle
-            ideal_ground
-            for l in line:
-                l1 = line[0:-2]
-                l2 = line[1:-1]
-                l3 = line[2:]
-                higher_than_ideal(l, length, ideal_ground)
-    else:
-        pass
+    if length % 2 and higher_than_ideal(line, ideal_ground):
+        return sum(line) - sum(ideal_ground)
+    wanted_s = None
+    while not wanted_s:
+        new_length, list_of_lines = create_params(length, line)
+        ideal_ground = create_ideal_ground(new_length)
+        for new_line in list_of_lines:
+            if higher_than_ideal(new_line, ideal_ground):
+                length = new_length
+                wanted_s = sum(line) - sum(ideal_ground)
+    return wanted_s
+
 
 def main():
     n = int(input())
     while n > 0:
-        length = input()
+        length = int(input())
         line = input()
-        number_of_operations(length, line)
+        print(number_of_operations(length, line))
         n -= 1
 
 
 if __name__ == '__main__':
     main()
-
-
-# 3
-
-
-# 3
-# 1 2 1
-
-# 4
-# 1 1 2 1
-
-# 5
-# 0 2 6 2 1
-# 1 2 3 2 1
-
-
-# 1 2 2 4 3 2 1
-# 1 2 3 2 1
-
-# 1 2 2 4 3 - -
-# - 2 2 4 3 2 -
-# - - 2 4 3 2 1
-
-# 0 1 2 3 3 2 1
-
-
-# max 3
-
-# 0
-
-# 1
-
-# 3
--- tests/solutions/test_a_temple_of_snakes.py
+++ tests/solutions/test_a_temple_of_snakes.py
@@ -1,15 +1,20 @@
 import pytest
 
-from solutions.a_temple_of_snakes import number_of_operations
+from solutions.a_temple_of_snakes import number_of_operations, create_list_of_lines
 
 EXAMPLES = (
     ('args', 'expected'),
     [
-        (('1 2 1', 3), 0),
-        # (('1 1 2 1', 4), 1),
-        (('1 2 6 2 1', 5), 3),
-        (('1 2 2 2 1', 5), None),
-        
+        ((3, '1 2 1'), 0),
+        ((5, '1 2 6 2 1'), 3),
+        ((5, '1 2 9 2 1'), 6),
+        ((7, '1 2 3 4 3 2 1'), 0),
+        ((7, '1 6 3 4 3 2 1'), 4),
+        ((5, '1 2 2 2 1'), 4),
+        ((7, '0 0 3 4 3 2 1'), 4),
+        ((5, '0 1 2 1 1'), 1),
+        ((5, '1 2 1 1 0'), 1),
+        ((4, '1 1 2 1'), 1),
     ]
 )
 
@@ -17,3 +22,13 @@ EXAMPLES = (
 @pytest.mark.parametrize(*EXAMPLES)
 def test_returns_correct_result(args, expected):
     assert number_of_operations(*args) == expected
+
+
+@pytest.mark.parametrize('line,new_length,expected', [
+    ([1, 2, 3, 4, 5], 3, [[1, 2, 3], [2, 3, 4], [3, 4, 5]]),
+    ([1, 2, 3, 4, 5, 6, 7], 3, [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7]]),
+    ([1, 2, 3, 4, 5, 6, 7], 5, [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7]]),
+    ([1, 2, 3, 4], 3, [[1, 2, 3], [2, 3, 4]]),
+])
+def test_create_list_of_lines(line, new_length, expected):
+    assert create_list_of_lines(line, new_length) == expected
