0d28ec09a7ddcd355d17752986b34aeec0d4b45c
alekseyaq <kuznetz.vrn@gmail.com>
2017-05-28 12:53:26 -0400

    slow solution for a_temple_of_snakes

--- solutions/a_temple_of_snakes.py
+++ solutions/a_temple_of_snakes.py
@@ -8,43 +8,27 @@ def create_ideal_ground(length):
 
 
 def higher_than_ideal(line, ideal_ground):
-    return all(block >= ideal_block
-               for block, ideal_block in zip(line, ideal_ground))
-
-
-def create_list_of_lines(line, new_length):
-    new_list_of_lines = []
-    length = len(line)
-    length_difference = length - new_length
-    for i in range(length_difference):
-        new_list_of_lines.append(line[i:-length_difference+i])
-    new_list_of_lines.append(line[-new_length:])
-    return new_list_of_lines
-
-
-def create_params(length, line):
-    if length % 2 == 0:
-        new_length = length - 1
-    else:
-        new_length = length - 2
-    list_of_lines = create_list_of_lines(line, new_length)
-    return new_length, list_of_lines
+    for block, ideal_block in zip(line, ideal_ground):
+        if block < ideal_block:
+            return False
+    return True
 
 
 def number_of_operations(length, line):
-    ideal_ground = create_ideal_ground(length)
-    line = list(map(int, line.split(' ')))
-    if length % 2 and higher_than_ideal(line, ideal_ground):
-        return sum(line) - sum(ideal_ground)
-    wanted_s = None
-    while not wanted_s:
-        new_length, list_of_lines = create_params(length, line)
-        ideal_ground = create_ideal_ground(new_length)
-        for new_line in list_of_lines:
-            if higher_than_ideal(new_line, ideal_ground):
-                length = new_length
-                wanted_s = sum(line) - sum(ideal_ground)
-    return wanted_s
+    line = list(map(int, line.split()))
+    sum_line = sum(line)
+    max_ideal_length = int(math.sqrt(sum_line)) * 2 - 1
+
+    for ideal_length in range(length, 0, -1):
+        ideal_ground = create_ideal_ground(ideal_length)
+        sum_ideal = sum(ideal_ground)
+        length_diff = length - ideal_length
+        for i in range(length_diff):
+            if higher_than_ideal(line[i:-length_diff+i], ideal_ground):
+                return sum_line - sum_ideal
+        last = line[-ideal_length:]
+        if higher_than_ideal(last, ideal_ground):
+            return sum_line - sum_ideal
 
 
 def main():
--- tests/solutions/test_a_temple_of_snakes.py
+++ tests/solutions/test_a_temple_of_snakes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from solutions.a_temple_of_snakes import number_of_operations, create_list_of_lines
+from solutions.a_temple_of_snakes import number_of_operations
 
 EXAMPLES = (
     ('args', 'expected'),
@@ -22,13 +22,3 @@ EXAMPLES = (
 @pytest.mark.parametrize(*EXAMPLES)
 def test_returns_correct_result(args, expected):
     assert number_of_operations(*args) == expected
-
-
-@pytest.mark.parametrize('line,new_length,expected', [
-    ([1, 2, 3, 4, 5], 3, [[1, 2, 3], [2, 3, 4], [3, 4, 5]]),
-    ([1, 2, 3, 4, 5, 6, 7], 3, [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7]]),
-    ([1, 2, 3, 4, 5, 6, 7], 5, [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7]]),
-    ([1, 2, 3, 4], 3, [[1, 2, 3], [2, 3, 4]]),
-])
-def test_create_list_of_lines(line, new_length, expected):
-    assert create_list_of_lines(line, new_length) == expected
