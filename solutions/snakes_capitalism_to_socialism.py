9aeb799c94c02d849afe12f2f9d2fc12739c3ca2
Pavel Karateev <karateev.pavel@ya.ru>
2017-06-01 15:05:01 +0300

    Faster solution for SNSOCIAL
    We now check if there were any difference after a step while looping
    over items in the grid and return the status back so there's no need
    for extra difference check

--- solutions/snakes_capitalism_to_socialism.py
+++ solutions/snakes_capitalism_to_socialism.py
@@ -1,31 +1,7 @@
-def get_max_diff_between_neighbours(list_, rows, cols, i, j):
-    rmin = i - 1 if i - 1 >= 0 else 0
-    rmax = i + 1 if i + 1 < rows else i
-    cmin = j - 1 if j - 1 >= 0 else 0
-    cmax = j + 1 if j + 1 < cols else j
-
-    diff_between_neighbours = []
-
-    for x in range(rmin, rmax + 1):
-        for y in range(cmin, cmax + 1):
-            diff_between_neighbours.append(list_[x][y] - list_[i][j])
-
-    return max(diff_between_neighbours)
-
-
-def get_difference(list_, rows, cols):
-    difference = []
-    
-    for i in range(rows):
-        for j in range(cols):
-            difference.append(get_max_diff_between_neighbours(list_, rows, cols, i, j))
-
-    return max(difference)
-
-
-def create_new_list(list_, rows, cols):
-
+def create_new_list(grid, rows, cols):
     new_list = [[] for _ in range(rows)]
+    changes = False
+
     for i in range(rows):
         for j in range(cols):
             rmin = i - 1 if i - 1 >= 0 else 0
@@ -36,21 +12,23 @@ def create_new_list(list_, rows, cols):
 
             for x in range(rmin, rmax + 1):
                 for y in range(cmin, cmax + 1):
-                    neighbours_wealth.append(list_[x][y])
+                    neighbours_wealth.append(grid[x][y])
             highest_wealth = max(neighbours_wealth)
-            
+
+            if grid[i][j] != highest_wealth:
+                changes = True
             new_list[i].append(highest_wealth)
 
-    return new_list
+    return changes, new_list
 
-def get_number_of_hours(list_, rows, cols):
-    list_ = [list(map(int, line.split())) for line in list_]
-    hours = 0
-    difference = get_difference(list_, rows, cols)
-    while difference > 0:
+
+def get_number_of_hours(grid, rows, cols):
+    grid = [[int(item) for item in line.split()] for line in grid]
+    hours = -1
+    changes = True
+    while changes:
         hours += 1
-        list_ = create_new_list(list_, rows, cols)
-        difference = get_difference(list_, rows, cols)
+        changes, grid = create_new_list(grid, rows, cols)
     return hours
 
 
@@ -58,10 +36,10 @@ def main():
     number_of_tests = int(input())
     for _ in range(number_of_tests):
         n, m = map(int, input().split())
-        list_ = []
+        grid = []
         for __ in range(n):
-            list_.append(input())
-        print(get_number_of_hours(list_, n, m))
+            grid.append(input())
+        print(get_number_of_hours(grid, n, m))
 
 
 if __name__ == '__main__':
